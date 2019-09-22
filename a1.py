903dd3fa1959442a1a0d95df48558123610e39d9
akhila1810 <55398476+akhila1810@users.noreply.github.com>
2019-09-22 16:16:29 -0300

    Delete a1.py

--- a1.py
+++
@@ -1,9 +0,0 @@
-y=open('file.txt', 'r')
-item_list = y.read().split()
-def no_e(word):
-    return word.find('e') == -1
-total = len(item_list)
-no_e = [word for word in item_list if no_e(word)]
-percentage = float(len(no_e)) / total
-print'{:.2%}'.format(percentage)
-
